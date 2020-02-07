4ff5e30d468b0b5abecb7b96e71542694085e303
you-ink <younkivr@gmail.com>
2020-02-07 09:37:30 +0700

    coba1

---
+++ latihan1.php
@@ -0,0 +1,20 @@
+<?php
+
+$mahasiswa = [
+  [
+    "nama" => "keyval",
+    "kelas" => "mabar",
+    "email" => "akulaku@gmail.com"
+  ],
+  [
+    "nama" => "nofil",
+    "kelas" => "makar",
+    "email" => "akulah@gmail.com"
+  ]
+];
+
+$data = json_encode($mahasiswa);
+echo $data;
+// 
+
+ ?>
